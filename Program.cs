512b2e70d2b4836688203ebe4ea094cc5b8b9980
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-09-02 18:36:53 -0300

    feat: Soluciona desafio implementando as classes filhas de Smartphone, Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "22335641", modelo: "NokiaN1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.SobreSmartphone();
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "44221365", modelo: "IphoneS2", imei: "2222222222", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+iphone.SobreSmartphone();
